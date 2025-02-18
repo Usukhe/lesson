15f46946be195740be9d65998fa4c3d3481b0cb2
Usukhe <osokhe@gmail.com>
2025-02-18 12:47:31 +0800

    java uzsenee nemlee

---
+++ javascript/javascript-01/main.js
@@ -0,0 +1,19 @@
+console.log("Сайн уу, JS");
+
+let message = "Hello, World"; // Striing
+console.log(message);
+
+let age = 42; // Number
+console.log(age)
+
+let gpa = 4.5; // Number
+console.log(gpa)
+
+let isStudent = true; // Boolean
+console.log(isStudent);
+
+let x; // Undefined
+console.log(x);
+
+let n = null; // Null
+console.log(n);
